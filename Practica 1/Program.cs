d60d1af345f431d1bd91409fe576ef818da82df1
faemv <faemv@FABIAN>
2023-10-15 19:39:06 -0600

    Agregar archivos de proyecto.

---
+++ Practica 1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using BiblioCarro;
+
+namespace Practica_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Automovil miAuto = new Automovil();
+        }
+    }
+}
